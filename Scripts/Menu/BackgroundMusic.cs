774fa83824f2d32e96a9cca44a6bd9d6780a69ac
Alex Luo <theaxluo@gmail.com>
2022-07-17 04:32:12 -0500

    background music

---
+++ Scripts/Menu/BackgroundMusic.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    [SerializeField] private List<AudioClip> music;
+    
+    private List<AudioClip> playlist;
+    private AudioSource source;
+    private int currIndex;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (source == null)
+        {
+            source = gameObject.GetComponent<AudioSource>();
+            Shuffle();
+            playlist = music;
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(!source.isPlaying)
+        {
+            source.clip = playlist[currIndex];
+            currIndex++;
+            source.Play();
+            if (currIndex >= playlist.Count)
+                SwapPlaylist();
+        }
+    }
+
+    private void SwapPlaylist()
+    {
+        playlist = music;
+        StartCoroutine(Shuffle());
+    }
+
+    private IEnumerator Shuffle()
+    {
+        List<AudioClip> newPlaylist = new List<AudioClip>();
+        while(music.Count > 0)
+        {
+            AudioClip selected = music[Random.Range(0, music.Count)];
+            music.Remove(selected);
+            newPlaylist.Add(selected);
+            yield return null;
+        }
+        music = newPlaylist;
+    }
+}
