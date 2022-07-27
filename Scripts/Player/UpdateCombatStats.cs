1fb0d8561095d4eb756bfdf329dd78979477413b
Alex Luo <theaxluo@gmail.com>
2022-07-26 23:02:31 -0500

    scuffed healthbar

--- Scripts/Player/UpdateCombatStats.cs
+++ Scripts/Player/UpdateCombatStats.cs
@@ -9,11 +9,28 @@ public class UpdateCombatStats : MonoBehaviour
     [SerializeField] private Text healthText;
     [SerializeField] private Text actionPointText;
     [SerializeField] private Text armorText;
+    [SerializeField] private RectTransform hpBar;
+    [SerializeField] private RectTransform hpBackground;
+    public Vector2 sizeDelta;
+
+    private Vector2 anchorPos;
+
+    private void Start()
+    {
+        anchorPos = hpBar.anchoredPosition;
+    }
 
     private void Update()
     {
         actionPointText.text = combatHandler.actionPoints.ToString() + "/" + combatHandler.maxActionPoints.ToString();
         armorText.text = combatHandler.armor.ToString();
         healthText.text = combatHandler.health.ToString() + "/" + combatHandler.maxHealth.ToString();
+
+        float percentHealth = (float) combatHandler.health / (float) combatHandler.maxHealth;
+        float newX = hpBackground.rect.width * percentHealth;
+        hpBar.sizeDelta = new Vector2(newX, 100f);
+        Vector2 newPos = anchorPos;
+        newPos.x -= (hpBackground.rect.width - newX) / 2f;
+        hpBar.anchoredPosition = newPos;
     }
 }
